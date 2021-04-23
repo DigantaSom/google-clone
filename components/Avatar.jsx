a161783518442151cf9bcd5bbccbc1de3f5a0a0c
Diganta Som <itsdsom111@gmail.com>
2021-04-23 22:01:19 +0530

    Set up homepage

---
+++ components/Avatar.jsx
@@ -0,0 +1,10 @@
+const Avatar = ({ url }) => (
+  <img
+    src={url}
+    alt='profile pic'
+    loading='lazy'
+    className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+  />
+);
+
+export default Avatar;
